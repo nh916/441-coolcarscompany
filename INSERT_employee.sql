6b24e9e416405a8dc7d1c0310a14298a8786c341
Navid Hariri <navidhariri25@gmail.com>
2019-11-11 16:31:08 -0800

    changed phone numeber

--- INSERT_employee.sql
+++ INSERT_employee.sql
@@ -1,2 +1,2 @@
 INSERT INTO employee (name, phone_number, email, date_hired, employee_type)
-VALUES ("navid", 8184038668, "gmail@gmail.com", "7/10/1993", "hourly");
+VALUES ("navid", 8186725432, "gmail@gmail.com", "7/10/1993", "hourly");
