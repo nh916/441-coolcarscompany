34f1603e7f99ec0c63e1af15706b2b9e65250b8a
Navid Hariri <navidhariri25@gmail.com>
2019-11-14 21:04:12 -0800

    pragma create changed to make more sense and new db

--- sql/CREATE_SCRIPT.sql
+++ sql/CREATE_SCRIPT.sql
@@ -3,10 +3,14 @@ CREATE TABLE IF NOT EXISTS customer
 (
     customer_id   INTEGER     NOT NULL,
     name          TEXT        NOT NULL,
+--     think about make it INT or varchar
+--     varchar for more flexibility
     phone_number  VARCHAR(30) NOT NULL,
+--     think about adding extension
     street        TEXT        NOT NULL,
     city          TEXT        NOT NULL,
-    state         TEXT        NOT NULL,
+-- allowing null for state in case of international
+    state         VARCHAR(2),
     zip_code      VARCHAR(20) NOT NULL,
     customer_type TEXT        NOT NULL,
     PRIMARY KEY (customer_id),
@@ -14,11 +18,11 @@ CREATE TABLE IF NOT EXISTS customer
 );
 
 
--- creation of domestic customer table with inheritance *CORRECT*
+-- creation of domestic customer table with inheritance
 CREATE TABLE IF NOT EXISTS domestic_customer
 (
     domestic_customer_id INTEGER NOT NULL,
-    regional_code        int     NOT NULL,
+    regional_code        INT     NOT NULL,
     PRIMARY KEY (domestic_customer_id),
     FOREIGN KEY (domestic_customer_id) REFERENCES customer (customer_id)
 );
@@ -28,7 +32,7 @@ CREATE TABLE IF NOT EXISTS domestic_customer
 CREATE TABLE IF NOT EXISTS international_customer
 (
     international_customer_id INTEGER NOT NULL,
-    country_code              int     NOT NULL,
+    country_code              INT     NOT NULL,
     PRIMARY KEY (international_customer_id),
     FOREIGN KEY (international_customer_id) REFERENCES customer (customer_id)
 );
@@ -44,26 +48,28 @@ CREATE TABLE IF NOT EXISTS employee
     date_hired    DATE DEFAULT CURRENT_DATE NOT NULL,
     employee_type TEXT        NOT NULL,
     PRIMARY KEY (employee_ID),
-    CHECK ( employee_type = 'hourly' or employee_type = 'salaried')
+    CHECK ( employee_type = 'sales' or employee_type = 'mechanic')
 );
 
 
-CREATE TABLE IF NOT EXISTS hourly_employee
+CREATE TABLE IF NOT EXISTS sales_person
 (
-    hourly_employee_id INTEGER NOT NULL,
-    hourly_salary_rate float   NOT NULL,
-    PRIMARY KEY (hourly_employee_id),
-    FOREIGN KEY (hourly_employee_id) REFERENCES employee (employee_ID)
+    employee_id INTEGER NOT NULL,
+    commission  FLOAT   NOT NULL,
+    vehicle_id  INTEGER NOT NULL,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (employee_id) REFERENCES employee (employee_ID),
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle (vehicle_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS salaried_employee
-(
-    salaried_employee_id INTEGER NOT NULL,
-    annual_rate          float   NOT NULL,
-    PRIMARY KEY (salaried_employee_id),
-    FOREIGN KEY (salaried_employee_id) REFERENCES employee (employee_ID)
-);
+-- CREATE TABLE IF NOT EXISTS salaried_employee
+-- (
+--     salaried_employee_id INTEGER NOT NULL,
+--     annual_rate          FLOAT   NOT NULL,
+--     PRIMARY KEY (salaried_employee_id),
+--     FOREIGN KEY (salaried_employee_id) REFERENCES employee (employee_ID)
+-- );
 
 CREATE TABLE IF NOT EXISTS employee_skill
 (
@@ -78,7 +84,7 @@ CREATE TABLE IF NOT EXISTS employee_skill
 CREATE TABLE IF NOT EXISTS mechanic
 (
     employee_id INTEGER NOT NULL,
-    hourly_rate float   NOT NULL,
+    hourly_rate FLOAT   NOT NULL,
     PRIMARY KEY (employee_id),
     FOREIGN KEY (employee_id) REFERENCES employee (employee_ID)
 );
@@ -99,25 +105,25 @@ CREATE TABLE IF NOT EXISTS vehicle
 (
     vehicle_id                           INTEGER NOT NULL,
     vehicle_name                         TEXT    NOT NULL,
-    vehicle_cost                         float   NOT NULL,
-    vehicle_sales_goal_for_current_year  int     NOT NULL,
-    vehicle_sales_goal_for_previous_year int     NOT NULL,
-    employee_id                          INTEGER NOT NULL,
-    PRIMARY KEY (vehicle_id),
-    FOREIGN KEY (employee_id) REFERENCES employee (employee_ID)
+    vehicle_cost                         FLOAT   NOT NULL,
+    vehicle_sales_goal_for_current_year  INT     NOT NULL,
+    vehicle_sales_goal_for_previous_year INT     NOT NULL,
+--     employee_id                          INTEGER NOT NULL,
+    PRIMARY KEY (vehicle_id)
+--     FOREIGN KEY (employee_id) REFERENCES employee (employee_ID)
 );
 
 
--- order line tabe
+-- create order line table
 CREATE TABLE IF NOT EXISTS order_line
 (
     order_line_id  INTEGER NOT NULL,
-    product_id     INTEGER NOT NULL,
     order_id       INTEGER NOT NULL,
-    order_quantity int     NOT NULL,
+    vehicle_id     INTEGER NOT NULL,
+    order_quantity INT     NOT NULL,
     PRIMARY KEY (order_line_id),
-    FOREIGN KEY (product_id) REFERENCES vehicle (vehicle_id),
-    FOREIGN KEY (order_id) REFERENCES orders (order_id)
+    FOREIGN KEY (order_id) REFERENCES orders (order_id),
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle (vehicle_id)
 );
 
 -- service table
@@ -135,39 +141,41 @@ CREATE TABLE IF NOT EXISTS service
 );
 
 -- sales person table
-CREATE TABLE IF NOT EXISTS salesPerson
-(
-    service_id INTEGER NOT NULL,
-    commission float   NOT NULL,
-    vehicle_id INTEGER NOT NULL,
-    PRIMARY KEY (service_id),
-    FOREIGN KEY (service_id) REFERENCES employee (employee_ID),
-    FOREIGN KEY (vehicle_id) REFERENCES vehicle (vehicle_id)
-);
+-- CREATE TABLE IF NOT EXISTS salesPerson
+-- (
+--     service_id INTEGER NOT NULL,
+--     commission FLOAT   NOT NULL,
+--     vehicle_id INTEGER NOT NULL,
+--     PRIMARY KEY (service_id),
+--     FOREIGN KEY (service_id) REFERENCES employee (employee_ID),
+--     FOREIGN KEY (vehicle_id) REFERENCES vehicle (vehicle_id)
+-- );
 
 -- part table
+-- todo fix this table and supplier
 CREATE TABLE IF NOT EXISTS part
 (
     part_id     INTEGER NOT NULL,
     part_name   TEXT    NOT NULL,
-    part_price  float   NOT NULL,
+    part_price  FLOAT   NOT NULL,
     mechanic_id INTEGER NOT NULL,
     primary key (part_id),
-    FOREIGN KEY (part_id) REFERENCES supplier (part_id),
+--     FOREIGN KEY (part_id) REFERENCES supplier (part_id),
     FOREIGN KEY (mechanic_id) REFERENCES mechanic (employee_id)
 );
 
 -- supplier table
 CREATE TABLE IF NOT EXISTS supplier
 (
-    supplier_id           INTEGER NOT NULL,
-    supplier_company_name TEXT    NOT NULL,
-    supplier_phone_number INT     NOT NULL,
-    street                TEXT    NOT NULL,
-    city                  TEXT    NOT NULL,
-    state                 TEXT    NOT NULL,
-    zip_code              INT     NOT NULL,
-    part_id               INTEGER NOT NULL,
-    PRIMARY KEY (supplier_id),
-    FOREIGN KEY (part_id) REFERENCES part (part_id)
+    supplier_id           INTEGER     NOT NULL,
+    supplier_company_name TEXT        NOT NULL,
+    supplier_phone_number VARCHAR(30) NOT NULL,
+    street                TEXT        NOT NULL,
+    city                  TEXT        NOT NULL,
+--     todo i think should allow for null in case international supplier
+    state                 VARCHAR(2),
+    zip_code              INT         NOT NULL,
+--     part_id               INTEGER     NOT NULL,
+    PRIMARY KEY (supplier_id)
+--     FOREIGN KEY (part_id) REFERENCES part (part_id)
 );
