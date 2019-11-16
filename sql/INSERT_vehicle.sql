1162ffcff0394585f922b92a88a8e8faf2f1ba9d
Navid Hariri <navidhariri25@gmail.com>
2019-11-16 14:35:09 -0800

    insert vehicle and new db

---
+++ sql/INSERT_vehicle.sql
@@ -0,0 +1,8 @@
+INSERT INTO vehicle(vehicle_make,
+                    vehicle_model,
+                    vehicle_year,
+                    vehicle_cost,
+                    vehicle_sales_goal_for_current_year,
+                    vehicle_sales_goal_for_previous_year
+                    )
+VALUES ('Lamborghini ', 'Aventador ', 2012, 417826.20, 10, 20);
