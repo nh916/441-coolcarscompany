9d9b3c0b87b73448a4fa28140252eab1b649047d
Navid Hariri <navidhariri25@gmail.com>
2019-11-21 15:28:08 -0800

    customer changed zip code and street together

--- sql_jj/customer_insertion -JJ.sql
+++ sql_jj/customer_insertion -JJ.sql
@@ -8,22 +8,22 @@ INSERT INTO customer(customer_name, customer_phone_number, customer_street, cust
 VALUES('Celia Young', 4238425859, 'Church customer_street', 30741, 'Wood River', 'domestic');
 
 INSERT INTO customer(customer_name, customer_phone_number, customer_street, customer_city, customer_zip_code, customer_type)
-VALUES('Wilson Goodwin', 5548581699, 'Fairview Avenue', 45840, 'Roy', 'domestic');
+VALUES('Wilson Goodwin', 5548581699, 'Fairview Avenue', 'Roy', 45840, 'domestic');
 
 INSERT INTO customer(customer_name, customer_phone_number, customer_street, customer_city, customer_zip_code, customer_type)
-VALUES('Leo Gill', 7184528143, 'Wall customer_street', 07026, 'Montross', 'domestic');
+VALUES('Leo Gill', 7184528143, 'Wall customer_street', 'Montross', 07026, 'domestic');
 
 INSERT INTO customer(customer_name, customer_phone_number, customer_street, customer_city, customer_zip_code, customer_type)
-VALUES('Reginald Hogan', 2833381181, 'Hudson customer_street', 11542, 'Marco Island', 'international');
+VALUES('Reginald Hogan', 2833381181, 'Hudson customer_street', 'Marco Island', 11542, 'international');
 
 INSERT INTO customer(customer_name, customer_phone_number, customer_street, customer_city, customer_zip_code, customer_type)
-VALUES('Elijah Joseph', 9239375532, 'Highland Avenue', 27526, 'Virgelle', 'international');
+VALUES('Elijah Joseph', 9239375532, 'Highland Avenue', 'Virgelle', 27526, 'international');
 
 INSERT INTO customer(customer_name, customer_phone_number, customer_street, customer_city, customer_zip_code, customer_type)
-VALUES('Katie Brady', 3958198655, 'Dogwood Drive', 16506, 'Bienville', 'international');
+VALUES('Katie Brady', 3958198655, 'Dogwood Drive', 'Bienville', 16506, 'international');
 
 INSERT INTO customer(customer_name, customer_phone_number, customer_street, customer_city, customer_zip_code, customer_type)
-VALUES('Mona Drake', 2179153374, 'Heritage Drive', 08527, 'Carthage', 'international');
+VALUES('Mona Drake', 2179153374, 'Heritage Drive', 'Carthage', 08527,'international');
 
 INSERT INTO customer(customer_name, customer_phone_number, customer_street, customer_city, customer_zip_code, customer_type)
-VALUES('Derrick Summers', 9768634277, '4th customer_street', 07726, 'Floyd', 'international');
+VALUES('Derrick Summers', 9768634277, '4th customer_street','Floyd', 07726, 'international');
