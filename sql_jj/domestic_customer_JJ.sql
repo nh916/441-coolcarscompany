b2b2689edf803337364a1fe9a191daa68e561952
Navid Hariri <navidhariri25@gmail.com>
2019-11-21 15:29:50 -0800

    changed domestic customer thing

--- sql_jj/domestic_customer_JJ.sql
+++ sql_jj/domestic_customer_JJ.sql
@@ -1,10 +1,10 @@
-INSERT INTO domestic_customer(domestic_customer_id, state)
+INSERT INTO domestic_customer(domestic_customer_id, domestic_customer_state)
 VALUES(1, 'CA');
-INSERT INTO domestic_customer(domestic_customer_id, state)
+INSERT INTO domestic_customer(domestic_customer_id, domestic_customer_state)
 VALUES(2, 'NY');
-INSERT INTO domestic_customer(domestic_customer_id, state)
+INSERT INTO domestic_customer(domestic_customer_id, domestic_customer_state)
 VALUES(3, 'NV');
-INSERT INTO domestic_customer(domestic_customer_id, state)
+INSERT INTO domestic_customer(domestic_customer_id, domestic_customer_state)
 VALUES(4, 'MA');
-INSERT INTO domestic_customer(domestic_customer_id, state)
+INSERT INTO domestic_customer(domestic_customer_id, domestic_customer_state)
 VALUES(5, 'CA');
