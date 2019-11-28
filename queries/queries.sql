42d99a60a57f7a016894be299548893f6cf50054
Navid Hariri <navidhariri25@gmail.com>
2019-11-27 20:03:16 -0800

    more sql quer

--- queries/queries.sql
+++ queries/queries.sql
@@ -28,3 +28,75 @@ WHERE vehicle_cost <
           SELECT AVG(vehicle_cost)
           FROM vehicle
       )
+
+
+
+
+SELECT COUNT(*) AS cars_priced_less_than_average
+FROM vehicle
+WHERE vehicle_cost <
+      (
+          SELECT AVG(vehicle_cost)
+          FROM vehicle
+      )
+UNION
+
+SELECT COUNT(*) AS total_vehicles
+FROM vehicle;
+
+
+
+
+-- what ratio of cars are priced more than the average price
+SELECT (
+    CAST(
+        (SELECT COUNT(*) AS cars_priced_less_than_average
+                        FROM vehicle
+WHERE vehicle_cost <
+(
+                                  SELECT AVG(vehicle_cost)
+                                  FROM vehicle
+                              )
+                       )
+                   AS FLOAT)
+               /
+               CAST(
+                       (
+                           SELECT COUNT(*)
+                           FROM vehicle
+                       )
+                   AS FLOAT
+                   )
+           )
+           AS ratio
+
+
+-- minified
+SELECT (CAST((SELECT COUNT(*) AS cars_priced_less_than_average
+              FROM vehicle
+              WHERE vehicle_cost < (SELECT AVG(vehicle_cost) FROM vehicle)) AS FLOAT) /
+        CAST((SELECT COUNT(*) FROM vehicle) AS FLOAT)) AS ratio
+
+
+
+-- SELECT COUNT(*)/total ratio
+-- FROM (SELECT v.*, avg(vehicle_cost) over() avgcost, COUNT (*) OVER() total FROM vehicle v) vout;
+
+
+
+
+
+-- what percentage of cars are priced less than average price round to 2 decimal places
+SELECT (ROUND(((CAST((SELECT COUNT(*) AS cars_priced_less_than_average
+                      FROM vehicle
+                      WHERE vehicle_cost < (SELECT AVG(vehicle_cost) FROM vehicle)) AS FLOAT) /
+                CAST((SELECT COUNT(*) FROM vehicle) AS FLOAT))) * 100, 2))
+
+
+
+
+
+
+
+
+
