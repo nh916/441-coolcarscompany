e12158c7a40932b429afe164102260258db3a611
Navid Hariri <navidhariri25@gmail.com>
2019-12-01 20:03:07 -0800

    changed employee skill from '' to '

--- sql used in project/Employee_skills_JJ.sql
+++ sql used in project/Employee_skills_JJ.sql
@@ -1,91 +1,91 @@
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (1, ''Product Knowledge'');
+VALUES (1, 'Product Knowledge');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (1, ''Strategic Prospecting employee_skills'');
+VALUES (1, 'Strategic Prospecting employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (1, ''Active Listening'');
+VALUES (1, 'Active Listening');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (2, ''Product Knowledge'');
+VALUES (2, 'Product Knowledge');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (2, ''Communication'');
+VALUES (2, 'Communication');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (2, ''Qualification Questioning'');
+VALUES (2, 'Qualification Questioning');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (3, ''Objection Handling'');
+VALUES (3, 'Objection Handling');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (3, ''Strategic Prospecting employee_skills'');
+VALUES (3, 'Strategic Prospecting employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (3, ''Rapport Building on the Call'');
+VALUES (3, 'Rapport Building on the Call');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (3, ''Qualification Questioning'');
+VALUES (3, 'Qualification Questioning');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (4, ''Product Knowledge'');
+VALUES (4, 'Product Knowledge');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (4, ''Buyer-Seller Agreement'');
+VALUES (4, 'Buyer-Seller Agreement');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (5, ''Objection Handling'');
+VALUES (5, 'Objection Handling');
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (5, ''Objection Prevention'');
+VALUES (5, 'Objection Prevention');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (5, ''Communication'');
+VALUES (5, 'Communication');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (6, ''Diagnostic employee_skills'');
+VALUES (6, 'Diagnostic employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (6, ''Customer service employee_skills'');
+VALUES (6, 'Customer service employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (6, ''Problem-solving employee_skills'');
+VALUES (6, 'Problem-solving employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (6, ''Technical aptitude'');
+VALUES (6, 'Technical aptitude');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (7, ''Customer service employee_skills'');
+VALUES (7, 'Customer service employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (7, ''Diagnostic employee_skills'');
+VALUES (7, 'Diagnostic employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (7, ''Technical aptitude'');
+VALUES (7, 'Technical aptitude');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (7, ''Problem-solving employee_skills'');
+VALUES (7, 'Problem-solving employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (8, ''Customer service employee_skills'');
+VALUES (8, 'Customer service employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (8, ''Diagnostic employee_skills'');
+VALUES (8, 'Diagnostic employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (9, ''Problem-solving employee_skills'');
+VALUES (9, 'Problem-solving employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (9, ''Technical aptitude'');
+VALUES (9, 'Technical aptitude');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (10, ''Customer service employee_skills'');
+VALUES (10, 'Customer service employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (10, ''Diagnostic employee_skills'');
+VALUES (10, 'Diagnostic employee_skills');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (10, ''Technical aptitude'');
+VALUES (10, 'Technical aptitude');
 
 INSERT INTO employee_skill (employee_id, employee_skill)
-VALUES (10, ''Problem-solving employee_skills'');
+VALUES (10, 'Problem-solving employee_skills');
